ed7ec196febcf3b5d6334d0a74c4672c89c3140c
Daniel <hi@swag.ultimateRIPMasters.onion>
2017-03-15 19:17:46 +0100

    Ai

---
+++ src/bomberman/ai/utility/PlayerRelevance.java
@@ -0,0 +1,35 @@
+package bomberman.ai.utility;
+
+import bomberman.gameplay.utils.*;
+import bomberman.gameplay.*;
+
+/**
+ * Created by 204g12 on 13.03.2017.
+ */
+public class PlayerRelevance{
+    private Player player;
+    private double relevance;
+
+    public PlayerRelevance(Player player){
+        this.player = player;
+        update();
+    }
+
+    public void update(){
+        //TODO: Inventar,Score etc.
+        relevance = player.getHealth();
+    }
+
+    public double getRelevance(){
+        return relevance;
+    }
+
+    public double getRelevance(Location location){
+        double dist = player.getBoundingBox().getCenter().distanceTo(location);
+        return relevance - dist;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+}
