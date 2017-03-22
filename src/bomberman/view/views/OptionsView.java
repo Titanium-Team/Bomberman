c5b885012375932d7209d822d3ffd35f7a75cde7
Leo1998 <leonard.fricke98@gmail.com>
2017-03-22 16:07:30 +0100

    fix

--- src/bomberman/view/views/OptionsView.java
+++ src/bomberman/view/views/OptionsView.java
@@ -4,6 +4,7 @@ import bomberman.Main;
 import bomberman.view.engine.Config;
 import bomberman.view.engine.ViewManager;
 import bomberman.view.engine.components.*;
+import org.lwjgl.opengl.Display;
 
 import java.util.Random;
 
@@ -13,6 +14,7 @@ import java.util.Random;
 public class OptionsView extends BaseMenuView {
 
     private Label titleLabel;
+    private Label infoLabel;
     private CheckBox fpsLabelCheckBox;
     private CheckBox vSyncCheckBox;
 
@@ -29,10 +31,18 @@ public class OptionsView extends BaseMenuView {
         this.fpsLabelCheckBox.addListener(() -> config.setShowFPS(fpsLabelCheckBox.isChecked()));
         this.getRoot().addChild(fpsLabelCheckBox);
 
-        this.vSyncCheckBox = new CheckBox(LayoutParams.obtain(0.4f, 0.45f, 0.1f, 0.1f), this, "vSync");
+        this.vSyncCheckBox = new CheckBox(LayoutParams.obtain(0.4f, 0.45f, 0.1f, 0.1f), this, "vSync*");
         this.vSyncCheckBox.setChecked(config.isvSync());
-        this.vSyncCheckBox.addListener(() -> config.setvSync(vSyncCheckBox.isChecked()));
+        this.vSyncCheckBox.addListener(new ClickListener() {
+            @Override
+            public void onClick() {
+                config.setvSync(vSyncCheckBox.isChecked());
+            }
+        });
         this.getRoot().addChild(vSyncCheckBox);
+
+        this.infoLabel = new Label(LayoutParams.obtain(0.4f, 0.9f, 0.2f, 0.1f), this, "* restart needed");
+        this.getRoot().addChild(infoLabel);
     }
 
 }
