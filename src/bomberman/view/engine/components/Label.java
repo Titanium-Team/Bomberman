d5d0b44c1e607cf95ad5cc05a2108e23d5425636
kingOfVikings <t.bolz99@gmail.com>
2017-03-30 19:15:52 +0200

    chat nicht mehr zentriert

--- src/bomberman/view/engine/components/Label.java
+++ src/bomberman/view/engine/components/Label.java
@@ -8,15 +8,21 @@ import bomberman.view.engine.rendering.Batch;
 public class Label extends ViewComponent {
 
     private String text;
+    private boolean alignLeft;
 
     public Label(LayoutParams params, View v, String text) {
         super(params, v);
         this.text = text;
+        this.alignLeft=false;
     }
 
     @Override
     public void draw(Batch batch) {
-        ViewManager.font.drawText(batch, text, (int) ((getX()) + (getWidth()) / 2 - ViewManager.font.getWidth(text) / 2), (int) ((getY()) + (getHeight()) / 2 - ViewManager.font.getLineHeight() / 2));
+        if (!alignLeft) {
+            ViewManager.font.drawText(batch, text, (int) ((getX()) + (getWidth()) / 2 - ViewManager.font.getWidth(text) / 2), (int) ((getY()) + (getHeight()) / 2 - ViewManager.font.getLineHeight() / 2));
+        }else{
+            ViewManager.font.drawText(batch, text, (int) getX(), (int) ((getY()) + (getHeight()) / 2 - ViewManager.font.getLineHeight() / 2));
+        }
     }
 
     public String getText() {
@@ -27,4 +33,7 @@ public class Label extends ViewComponent {
         this.text = text;
     }
 
+    public void setAlignLeft(boolean alignLeft) {
+        this.alignLeft = alignLeft;
+    }
 }
